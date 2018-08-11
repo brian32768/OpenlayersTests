72f1e141098ce6b92f42ef8135367a34cd76b7dd
Brian Wilson <brian@wildsong.biz>
2018-08-10 22:43:14 -0700

    initial

---
+++ openlayers5.js
@@ -0,0 +1,17 @@
+﻿// Inspired by the book "OpenLayers 3.x Cookbook Second Edition"
+
+console.log("umma gumma");
+
+var map = new ol.Map({
+    view: new ol.View({
+        center: [-13750000, 5800000],
+        zoom: 9
+    }),
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    target: 'js-map'
+});
+
