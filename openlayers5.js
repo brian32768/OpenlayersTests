884fd36558e4c7584e09b4fb7eba5288e4084e0d
Brian Wilson <bwilson@co.clatsop.or.us>
2018-08-13 16:16:29 -0700

    separating into projects

--- openlayers5.js
+++ openlayers5.js
@@ -1,18 +1,21 @@
 ﻿// Inspired by the book "OpenLayers 3.x Cookbook Second Edition"
 
-require "node_modules/ol/Map.js";
-require "node_modules/ol/View.js";
-require "node_modules/ol/layer/Tile.js";
-require "node_modules/ol/source.js";
+// This has to be run via parcel
+// which uses node to translate these imports to something usable in a browser
 
-var map = new ol.Map({
-    view: new ol.View({
+import Map from "node_modules/ol/Map.js";
+import View from "node_modules/ol/View.js";
+import Tile from "node_modules/ol/layer/Tile.js";
+import OSM from "node_modules/ol/source.js";
+
+var map = new Map({
+    view: new View({
         center: [-13750000, 5800000],
         zoom: 9
     }),
     layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
+        new Tile({
+            source: new OSM()
         })
     ],
     target: 'js-map'
