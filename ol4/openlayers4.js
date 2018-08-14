a349365fca60dc0288b8e443bf1986436142e616
Brian Wilson <bwilson@co.clatsop.or.us>
2018-08-13 17:06:41 -0700

    added v4 again

---
+++ ol4/openlayers4.js
@@ -0,0 +1,15 @@
+﻿// Inspired by the book "OpenLayers 3.x Cookbook Second Edition"
+
+var map = new ol.Map({
+    view: new ol.View({
+        center: [-13750000, 5800000],
+        zoom: 9
+    }),
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    target: 'js-map'
+});
+
